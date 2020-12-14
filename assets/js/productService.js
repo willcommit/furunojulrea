2011bc615764f183618511fb3a8acb677504403f
William Elfversson <welfversson@gmail.com>
2020-12-14 13:07:56 +0100

    adds snowflakes animation

--- assets/js/productService.js
+++ assets/js/productService.js
@@ -55,6 +55,8 @@ fetch(url)
     })
   })
 
+
+//MODAL Logic
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("info-btn");
 var span = document.getElementsByClassName("close")[0];
@@ -63,12 +65,10 @@ btn.onclick = function () {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
